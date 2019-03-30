adc5426621b912f0a5c23280b78f8cb6def35a0d
James Merkin <jnmerk@gmail.com>
2019-03-30 18:58:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(obj, key, value){
-    Object.assign({},obj[key]=value);
-    return {};
-    
-  }
+    Object.assign({},obj{[key]=value});
+
 
 function destructivelyUpdateObjectWithKeyAndValue (object,key,value){
   object[key]=value;
