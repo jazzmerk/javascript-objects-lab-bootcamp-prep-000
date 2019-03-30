1295de963a2492088cb4e4d054320472ebde9359
James Merkin <jnmerk@gmail.com>
2019-03-30 19:19:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function destructivelyUpdateObjectWithKeyAndValue (object,key,value){
   }
  
 function destructivelyDeleteFromObjectByKey (object,key){
-  delete object.key;
+  delete object[key];
   
 }
