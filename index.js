a14e294a01128818b7cd7276a461a79d4b61d147
James Merkin <jnmerk@gmail.com>
2019-03-30 19:13:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ function updateObjectWithKeyAndValue(object, key, value){
     return Object.assign({}, object,{[key]:value} ); 
   }
   
+  function deleteFromObjectByKey (object,key){
   
+  assign.Object ({},object);
+  delete {
+  
+  }
 function destructivelyUpdateObjectWithKeyAndValue (object,key,value){
   object[key]=value;
   return object;
